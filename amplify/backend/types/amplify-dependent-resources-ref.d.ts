279e3fad1f81c89b63e8df7c6f508c5b8b93577f
web3ten0 <web3ten0@gmail.com>
2023-02-08 09:29:51 +0900

    added amplify auth, storage, api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "next1": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "next16574e18e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "next1": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
