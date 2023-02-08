f35155426e533773b2866075a52ea6d621110975
web3ten0 <web3ten0@gmail.com>
2023-02-08 21:03:35 +0900

    react-hook-formで値取得

--- src/pages/todos/create.tsx
+++ src/pages/todos/create.tsx
@@ -1,18 +1,34 @@
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { createTodo } from "@/graphql/mutations";
 import "@aws-amplify/ui-react/styles.css";
+import { Text, Input, Textarea, Button } from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface Props {
   signOut: React.ReactNode;
   user: React.ReactNode;
 }
 
-function Login({ signOut, user }: Props) {
+interface TodoFormInput {
+  title: string;
+  description: string;
+}
+
+function CreateTodo({ user }: Props) {
+  const { register, handleSubmit } = useForm<TodoFormInput>();
+  const onSubmit: SubmitHandler<TodoFormInput> = (data) => console.log(data);
   return (
-    <div>
-      <div>LOGIN</div>
-      <p>This is LOGIN page</p>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Text mb="8px">Title</Text>
+      <Input {...register("title")} />
+      <Text mb="8px" mt="10px">
+        Description
+      </Text>
+      <Textarea {...register("description")} />
+      <Button colorScheme="teal" mt="20px" type="submit">
+        保存
+      </Button>
+    </form>
   );
 }
-export default withAuthenticator(Login);
+export default withAuthenticator(CreateTodo);
