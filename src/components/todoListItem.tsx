6bade178190303ab2a47bea98297934b9e1798c5
web3ten0 <web3ten0@gmail.com>
2023-02-09 08:39:11 +0900

    delete todo

--- src/components/todoListItem.tsx
+++ src/components/todoListItem.tsx
@@ -1,13 +1,25 @@
 import { Todo as TodoType } from "@/API";
+import { Text, Divider } from "@chakra-ui/react";
+import { deleteTodo } from "@/graphql/mutations";
+import { API } from "aws-amplify";
 
 interface TodoProps {
   todo: TodoType;
 }
 
 export default function TodoListItem({ todo }: TodoProps) {
+  const del = async (id: string) => {
+    return await API.graphql({
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+      query: deleteTodo,
+      variables: { input: { id } },
+    });
+  };
   return (
     <li>
       {todo.name}:{todo.description}
+      <Text onClick={() => del(todo.id)}>delete</Text>
+      <Divider />
     </li>
   );
 }
